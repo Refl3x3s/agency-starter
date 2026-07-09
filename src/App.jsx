97305491056fec964e1f7850c27156fe6f3dd1bb
Refl3x3s <aaronaabril@gmail.com>
2026-07-09 15:32:13 -0500

    initial vite project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+
+      <main>
+        <h1>Premium websites powered by AI.</h1>
+      </main>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+function Navbar() {
+    return (
+      <nav>
+        <h2>Project Atlas</h2>
+      </nav>
+    );
+  }
+  
+  export default Navbar;
